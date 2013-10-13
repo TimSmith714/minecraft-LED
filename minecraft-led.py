70447dc8eae1a507c93cb1ab1d10390591347bdb
TimSmith714 <timsmith714@gmail.com>
2013-10-13 23:42:44 +0100

    First edit in pycharm

--- minecraft-led.py
+++ minecraft-led.py
@@ -4,17 +4,19 @@
 import minecraft.minecraft as minecraft
 import minecraft.block as block
 import time
-import RPi.GPIO as GPIO
 
+#Suggested code from GPIO google code site as easy to forget to run as root
+#https://code.google.com/p/raspberry-gpio-python/wiki/BasicUsage
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
 #Set GPIO to use the actual numbers NOT pins
 GPIO.setmode(GPIO.BCM)
 
 #Set GPIO Ports for LEDs
-#17 Red
 RED = 17
-#27 Yellow
 YELLOW = 27
-#22 Green
 GREEN = 22
 
 #Setup the GPIO Pins
@@ -39,13 +41,32 @@ if __name__ == "__main__":
 
 
     #Take that position data and subtract 1 from y value to get the position of the block below
-    blockPos =
+    blockType = world.getBlock(playerPos.x, playerPos.y - 1, playerPos.z)
 
-
-#Discover the block type at that location
+    if blockType == 0:
+        GPIO.output(RED, True)
+        GPIO.output(YELLOW, False)
+        GPIO.output(GREEN, False)
+    elif blockType == 8:
+        GPIO.output(RED, False)
+        GPIO.output(YELLOW, True)
+        GPIO.output(GREEN, False)
+    elif blockType == 9:
+        GPIO.output(RED, False)
+        GPIO.output(YELLOW, True)
+        GPIO.output(GREEN, False)
+    else:
+        GPIO.output(RED, True)
+        GPIO.output(YELLOW, False)
+        GPIO.output(GREEN, False)
 
 
 #If block is
 #   Air -> Shine Red LED  |  Dec 0 Hex 0
+#   GPIO.output(RED, True)
+
 #   Water -> Shine Yellow LED |  Dec 8 OR 9  Hex 8 OR 9
+#   GPIO.output(YELLOW, True)
+
 #   Any other block -> Shine Green LED
+#   GPIO.output(GREEN, True)
